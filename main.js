bdd6b5a612766aa5e8923d7e40d33e9dc63fe4d2
andriamgra <andriamarana.f@zurcher.edu.mg>
2026-03-23 20:50:41 +0300

    Mise à jour du dashboard

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const container = document.getElementById("cards");
+const refreshBtn = document.getElementById("refreshBtn");
+
 const createCard = (title, content) => {
     return `
-
-<div class="card">
-<h2>${title}</h2>
-<p>${content}</p>
-</div>
-`;
+        <div class="card">
+            <h2>${title}</h2>
+            <p>${content}</p>
+        </div>
+    `;
 };
-
-const fetchData = async () => {
-    const res = await fetch("http://localhost:3000/api/data");
-    const data = await res.json();
+// afficher les cartes
+const renderCards = (data) => {
     container.innerHTML = data.map(item =>
         createCard(item.title, item.content)
     ).join("");
 };
+const fetchData = async () => {
+    try {
+        const res = await fetch("http://localhost:3000/api/data");
+        const data = await res.json();
+        renderCards(data);
+    } catch (error) {
+        container.innerHTML = "Erreur de chargement";
+    }
+
+};
+
+refreshBtn.addEventListener("click", fetchData);
 fetchData();
